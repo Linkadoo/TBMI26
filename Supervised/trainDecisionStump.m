4fb6309c44767874a5519155a185f3f4854cf83b
Linus Ahlenius <linah499@student.liu.se>
2017-02-08 21:35:56 +0100

    lab2 emin

---
+++ Supervised/trainDecisionStump.m
@@ -0,0 +1,35 @@
+function [threshMin, polarityMin, errorVec, emin] = trainDecisionStump( featureMatrix, Y, weights)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+for k = 1:size(featureMatrix,1)
+    
+    kthFeature = featureMatrix(k,:)';
+    
+    kthFeature2 = sort(kthFeature);
+    kthFeature2 = [kthFeature(1)-1; kthFeature; kthFeature(end)+1];
+    
+    emin = inf;
+    for j = 1:size(kthFeature2,1)-1
+       polarity = 1;
+       tempThresh = (kthFeature2(j) + kthFeature2(j+1))/2;
+       [errorTemp errorTempV] = weakClassifier(kthFeature,Y,tempThresh,weights); 
+       
+       if(errorTemp > 0.5)
+            polarity = -1;
+            errorTemp = 1 - errorTemp;
+     
+           
+       end
+       if(errorTemp < emin)
+           threshMin = tempThresh;
+           emin = errorTemp;
+           polarityMin = polarity;
+           errorVec = errorTempV;
+       end
+        
+    end
+
+
+end
+
---
+++ boosting/evaluateBoosting.m
@@ -0,0 +1,36 @@
+%% Load face and non-face data and plot a few examples
+load faces, load nonfaces
+faces = double(faces); nonfaces = double(nonfaces);
+figure(1)
+colormap gray
+for k=1:25
+subplot(5,5,k), imagesc(faces(:,:,10*k)), axis image, axis off
+end
+figure(2)
+colormap gray
+for k=1:25
+subplot(5,5,k), imagesc(nonfaces(:,:,10*k)), axis image, axis off
+end
+%% Generate Haar feature masks
+
+nbrHaarFeatures = 100;
+haarFeatureMasks = GenerateHaarFeatureMasks(nbrHaarFeatures);
+figure(3)
+colormap gray
+for k = 1:25
+subplot(5,5,k),imagesc(haarFeatureMasks(:,:,k),[-1 2])
+axis image,axis off
+end
+%% Create a training data set with a number of training data examples
+% from each class. Non-faces = class label y=-1, faces = class label y=1
+nbrTrainExamples = 2000;
+trainImages = cat(3,faces(:,:,1:nbrTrainExamples),nonfaces(:,:,1:nbrTrainExamples));
+xTrain = ExtractHaarFeatures(trainImages,haarFeatureMasks);
+yTrain = [ones(1,nbrTrainExamples), -ones(1,nbrTrainExamples)];
+
+%%
+[alfa, thresh, polarity] = trainBoosting(xTrain, yTrain, 4);
+%%
+Lclass = strongClassifier(xTrain,polarity, thresh, alfa);
+cM = calcConfusionMatrix(Lclass,yTrain);
+acc = calcAccuracy(cM)
